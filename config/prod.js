044f353bd67bc405e3f493ea071782b6e3b321a4
Kritartha Ghosh <kritartha@gmail.com>
2018-12-12 19:48:18 +0530

    changes in config keys

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+	GoogleClientID: process.env.GoogleClientID,
+	GoogleClientSecret: process.env.GoogleClientSecret,
+	mongoURI: process.env.mongoURI,
+	cookieKey: process.env.mongoURI
+};
